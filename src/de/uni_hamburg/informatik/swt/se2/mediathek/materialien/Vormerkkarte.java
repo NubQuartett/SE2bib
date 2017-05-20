d0c6d291682613e0b580f645414bf078c4332513
emyla <helen_schloh@web.de>
2017-05-20 22:58:10 +0200

    Add files via upload

---
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/Vormerkkarte.java
@@ -0,0 +1,34 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.materialien;
+
+public class Vormerkkarte
+{
+    private Kunde _vormerker1;
+    private Kunde _vormerker2;
+    private Kunde _vormerker3;
+    
+    public Kunde getVormerker2()
+    {
+        return _vormerker2;
+    }
+    public void setVormerker2(Kunde _vormerker2)
+    {
+        this._vormerker2 = _vormerker2;
+    }
+    public Kunde getVormerker1()
+    {
+        return _vormerker1;
+    }
+    public void setVormerker1(Kunde _vormerker1)
+    {
+        this._vormerker1 = _vormerker1;
+    }
+    public Kunde getVormerker3()
+    {
+        return _vormerker3;
+    }
+    public void setVormerker3(Kunde _vormerker3)
+    {
+        this._vormerker3 = _vormerker3;
+    }
+
+}
---
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/VormerkkarteTest.java
@@ -0,0 +1,18 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.materialien;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class VormerkkarteTest {
+	
+	public VormerkkarteTest()
+	{
+		
+	}
+	
+	
+
+}
