b99bfe12ae4f386cdc333e7b231e29fa95ba761f
emyla <helen_schloh@web.de>
2017-05-21 20:12:17 +0200

    Update VormerkkartenService.java

--- src/de/uni_hamburg/informatik/swt/se2/mediathek/services/VormerkkartenService.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/services/VormerkkartenService.java
@@ -22,26 +22,6 @@ public class VormerkkartenService
         return true;
     }
     
-    //TODO rückmeldung ob geklappt hat?, schlechte implementierung
-    //
-    public void neueVormerkungHinzufuegen(Medium medium, Kunde kunde)
-    {
-    	assert medium != null : "Vorbedingung verletzt: medium != null";
-    	assert kunde != null : "Vorbedingung verletzt: kunde != null";
-    	assert medium.getVormerkkarte().getVormerker3() == null : 
-    		"Vorbedingung verletzt : Vormerker3 == null";
-    	
-        Vormerkkarte vormerkkarte = medium.getVormerkkarte();
-        if(vormerkkarte.getVormerker1() == null)
-        {
-            vormerkkarte.setVormerker1(kunde);
-        }
-        else if(vormerkkarte.getVormerker2() == null)
-        {
-            vormerkkarte.setVormerker2(kunde);
-        }
-        else vormerkkarte.setVormerker3(kunde);
-    }
     
     public boolean istVormerkkarteLeer(Medium medium)
     {
